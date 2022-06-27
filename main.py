0d082ff2750795006e4ba73d2bf28b8876727d28
fs <fs@gundp.ch>
2022-06-27 15:52:09 +0200

    Initial commit

--- main.py
+++ main.py
@@ -125,10 +125,100 @@ if __name__ == '__main__':
 
 
     plot_history(history)
-
-
-
     print("Validation:", model.evaluate(val_images,val_labels))
+
+    print(30*"*")
+    # define cnn model
+    def define_model():
+        # We begin by defining the a empty stack. We'll use this for building our
+        # network, later by layer.
+        model = tf.keras.models.Sequential()
+
+        # We start with a convolutional layer this will extract features from
+        # the input images by sliding a convolution filter over the input image,
+        # resulting in a feature map.
+        model.add(
+            tf.keras.layers.Conv2D(
+                filters=32,  # How many filters we will learn
+                kernel_size=(3, 3),  # Size of feature map that will slide over image
+                strides=(1, 1),  # How the feature map "steps" across the image
+                padding='valid',  # We are not using padding
+                activation='relu',  # Rectified Linear Unit Activation Function
+                input_shape=(28, 28, 1)  # The expected input shape for this layer
+            )
+        )
+
+        # The next layer we will add is a Maxpooling layer. This will reduce the
+        # dimensionality of each feature, which reduces the number of parameters that
+        # the model needs to learn, which shortens training time.
+        model.add(
+            tf.keras.layers.MaxPooling2D(
+                pool_size=(2, 2),  # Size feature will be mapped to
+                strides=(2, 2)  # How the pool "steps" across the feature
+            )
+        )
+
+        # We'll now add a dropout layer. This fights overfitting and forces the model to
+        # learn multiple representations of the same data by randomly disabling neurons
+        # in the learning phase.
+        model.add(
+            tf.keras.layers.Dropout(
+                rate=0.25  # Randomly disable 25% of neurons
+            )
+        )
+
+        # Output from previous layer is a 3D tensor. This must be flattened to a 1D
+        # vector before beiung fed to the Dense Layers.
+        model.add(
+            tf.keras.layers.Flatten()
+        )
+
+        # A dense (interconnected) layer is added for mapping the derived features
+        # to the required class.
+        model.add(
+            tf.keras.layers.Dense(
+                units=128,  # Output shape
+                activation='relu'  # Rectified Linear Unit Activation Function
+            )
+        )
+
+        # Final layer with 10 outputs and a softmax activation. Softmax activation
+        # enables me to calculate the output based on the probabilities.
+        # Each class is assigned a probability and the class with the maximum
+        # probability is the model’s output for the input.
+        model.add(
+            tf.keras.layers.Dense(
+                units=10,  # Output shape
+                activation='softmax'  # Softmax Activation Function
+            )
+        )
+
+        # Build the model
+        model.compile(
+            loss=tf.keras.losses.sparse_categorical_crossentropy,  # loss function
+            optimizer=tf.keras.optimizers.Adam(),  # optimizer function
+            metrics=['accuracy']  # reporting metric
+        )
+
+        # Display a summary of the models structure
+        model.summary()
+        return model
+
+
+    model_RCN = define_model()
+
+    history = model_RCN.fit(training_images, training_labels, epochs=10,
+                            # batch_size=32,
+                            validation_data=(test_images, test_labels))
+    # evaluate model
+    _, acc = model_RCN.evaluate(test_images, test_labels, verbose=0)
+    print('> %.3f' % (acc * 100.0))
+
+
+    plot_history(history)
+
+
+
 
     plt.show()
 
